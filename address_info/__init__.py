778e81dbe9222378d402952f14957437d1422984
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2020-11-11 08:33:15 +0100

    Update __init__.py

--- address_info/__init__.py
+++ address_info/__init__.py
@@ -84,31 +84,3 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=8080, threaded=False)
-
-
-
-# api-endpoint
-#URL = "http://maps.googleapis.com/maps/api/geocode/json"
-
-# location given here
-#location = "delhi technological university"
-#appkey = "AIzaSyA33olk9yeGwJ8pevkPGdd4t5yLnYno_wc"
-
-# defining a params dict for the parameters to be sent to the API
-#PARAMS = {'address':location,'key':appkey}
-
-# sending get request and saving the response as response object
-#r = requests.get(url = URL, params = PARAMS)
-
-# extracting data in json format
-#data = r.json()
-
-
-# extracting latitude, longitude and formatted address
-# of the first matching location
-#latitude = data['results'][0]['geometry']['location']['lat']
-#longitude = data['results'][0]['geometry']['location']['lng']
-#formatted_address = data['results'][0]['formatted_address']
-
-# printing the output
-#return "Latitude:%s\nLongitude:%s\nFormatted Address:%s" %(latitude, longitude,formatted_address)
